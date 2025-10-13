d6a79a9a7a4085ea9dddd2de6bf0b0211cf66ad8
Utopia_M <myj0210@mail.ustc.edu.cn>
2025-10-13 11:45:54 +0800

    Add files via upload

---
+++ colour_scheme.R
@@ -0,0 +1,16 @@
+### 20251010
+
+colour_scheme<-list(
+  "blue"="#1B38A6",#rgb(27, 56, 166)
+  "red"="#F4270C",#rgb(244, 39, 12)
+  "orange"="#F4AD0C",#rgb(244, 173, 12)
+  "green"="#09B636",#rgb(9, 182, 54)
+  "yellow"="#F1DD10",#rgb(241, 221, 16)
+  "purple"="#C68EFD",#rgb(198, 142, 253)
+  "hot pink"="#FF0066",#rgb(255, 0, 102)
+  "light blue"="#75C2F6",#rgb(117, 194, 246)
+  "light red"="#FF6A56",#rgb(255, 106, 86)       # The red ones are unified with this, but the heatmap ones remain unchanged.
+  "dark red"="#A31300",#rgb(163, 19, 0)
+  "dark green"="#007A20",#rgb(0, 122, 32)
+  "pink"="#FFB0A5" #rgb(255, 176, 165)
+)
