c9303ce5e36d9ceea3cfe076b794f8bf76344fd6
Utopia_M <myj0210@mail.ustc.edu.cn>
2025-10-25 18:42:17 +0800

    Add files via upload

---
+++ 20251025_step_of_new_f6.R
@@ -0,0 +1,54 @@
+### 20251025
+
+## 主图三个视角 without energy
+
+
+## RAF1 front-K13 back-merge/K13 front-RAF1-merge/RAF1 right-K13 left-merge
+
+## open "C:\\Users\\36146\\OneDrive - USTC\\DryLab\\base_information_for_K13_K19_project\\6vjj.pdb"
+## open "C:/Users/36146/OneDrive - USTC/DryLab/base_information_for_K13_K19_project/6h46.pdb"
+## set bgColor #ffffff00;matchmaker #2 to #1;color #1/B black;color #2/B black;show #1/A surface;color #1/A dimgrey;hide #2/A cartoon;hide solvent;lighting soft;graphics silhouettes true color black width 1;cartoon style width 2.5 thickness 0.8;
+## save "C:/Users/36146/OneDrive - USTC/Manuscripts/K13_K19/figures/figure6/20251025/RAF1_front_K13_back.cxs"
+## turn y 200;turn x 340
+## save "C:/Users/36146/OneDrive - USTC/Manuscripts/K13_K19/figures/figure6/20251025/K13_front_RAF1_back.cxs"
+## hide atoms
+## save "C:/Users/36146/OneDrive - USTC/Manuscripts/K13_K19/figures/figure6/20251025/RAF1_right_K13_left.cxs"
+
+
+## endow median to structure
+
+## open "C:\\Users\\36146\\OneDrive - USTC\\Manuscripts\\K13_K19\\figures\\figure6\\20251025\\RAF1_front_K13_back.cxs"
+## open "C:/Users/36146/OneDrive - USTC/DryLab/Data_analysis_scripts/median ddG endow different type structure/results/20250904_median for structure/median/6h46_K13_median_ddG_use_0901_data.pdb" 
+## open "C:/Users/36146/OneDrive - USTC/DryLab/Data_analysis_scripts/median ddG endow different type structure/results/20250904_median for structure/median/6vjj_RAF1_median_ddG_use_0901_data_2.pdb"
+## matchmaker #2-3#!4 to #1;sel #4/B;color sel black;sel #4/A;surface sel;color bfactor #4/A range -1,1;hide solvent;lighting soft;graphics silhouettes true color black width 1;cartoon style width 2.5 thickness 0.8;
+## save "C:/Users/36146/OneDrive - USTC/Manuscripts/K13_K19/figures/figure6/20251025/RAF1_front with 0901 median ddG.cxs"
+## sel #3/B;color sel black;sel #3/A;surface sel;color bfactor #3/A range -1,1;
+## save "C:/Users/36146/OneDrive - USTC/Manuscripts/K13_K19/figures/figure6/20251025/K13_back with 0901 median ddG.cxs"
+
+
+
+## open "C:\\Users\\36146\\OneDrive - USTC\\Manuscripts\\K13_K19\\figures\\figure6\\20251025\\K13_front_RAF1_back.cxs"
+## open "C:/Users/36146/OneDrive - USTC/DryLab/Data_analysis_scripts/median ddG endow different type structure/results/20250904_median for structure/median/6h46_K13_median_ddG_use_0901_data.pdb"
+## open "C:/Users/36146/OneDrive - USTC/DryLab/Data_analysis_scripts/median ddG endow different type structure/results/20250904_median for structure/median/6vjj_RAF1_median_ddG_use_0901_data_2.pdb"
+## matchmaker #2-3#!4 to #1;sel #4/B;color sel black;sel #4/A;surface sel;color bfactor #4/A range -1,1;hide solvent;lighting soft;graphics silhouettes true color black width 1;cartoon style width 2.5 thickness 0.8;
+## save "C:/Users/36146/OneDrive - USTC/Manuscripts/K13_K19/figures/figure6/20251025/RAF1_back with 0901 median ddG.cxs"
+## sel #3/B;color sel black;sel #3/A;surface sel;color bfactor #3/A range -1,1;
+## save "C:/Users/36146/OneDrive - USTC/Manuscripts/K13_K19/figures/figure6/20251025/K13_front with 0901 median ddG.cxs"
+
+
+## open "C:\\Users\\36146\\OneDrive - USTC\\Manuscripts\\K13_K19\\figures\\figure6\\20251025\\RAF1_right_K13_left.cxs"
+## open "C:/Users/36146/OneDrive - USTC/DryLab/Data_analysis_scripts/median ddG endow different type structure/results/20250904_median for structure/median/6h46_K13_median_ddG_use_0901_data.pdb"
+## open "C:/Users/36146/OneDrive - USTC/DryLab/Data_analysis_scripts/median ddG endow different type structure/results/20250904_median for structure/median/6vjj_RAF1_median_ddG_use_0901_data_2.pdb"
+## matchmaker #2-3#!4 to #1;sel #4/B;color sel black;sel #4/A;surface sel;color bfactor #4/A range -1,1;hide solvent;lighting soft;graphics silhouettes true color black width 1;cartoon style width 2.5 thickness 0.8;
+## save "C:/Users/36146/OneDrive - USTC/Manuscripts/K13_K19/figures/figure6/20251025/RAF1_right with 0901 median ddG.cxs"
+## sel #3/B;color sel black;sel #3/A;surface sel;color bfactor #3/A range -1,1;
+## save "C:/Users/36146/OneDrive - USTC/Manuscripts/K13_K19/figures/figure6/20251025/K13_left with 0901 median ddG.cxs"
+
+
+
+
+
+
+
+
+
